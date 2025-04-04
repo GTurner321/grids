d97e2e160926a86a52c2860348794dd295297f27
GTurner321 <george.turner@jumeirahcollege.com>
2025-04-04 20:29:09 +0400

    Update gamecontroller.js

--- js/gamecontroller.js
+++ js/gamecontroller.js
@@ -33,18 +33,6 @@ class GameController {
         addBorderStyles();
         
         document.querySelector('.game-container')?.classList.remove('game-active');
-
-const gameStartEvent = new CustomEvent('gameStart', {
-    detail: { level: level }
-});
-window.dispatchEvent(gameStartEvent);
-console.log('Dispatched gameStart event for level', level);
-
-// Also refresh bottom buttons visibility if leaderboardManager exists
-if (window.leaderboardManager && typeof window.leaderboardManager.checkButtonVisibility === 'function') {
-    setTimeout(() => window.leaderboardManager.checkButtonVisibility(), 50);
-    setTimeout(() => window.leaderboardManager.checkButtonVisibility(), 300);
-}
         
         // Only initialize event listeners if DOM is fully loaded
         if (document.readyState === 'loading') {
